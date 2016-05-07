da06871c9d0022f86f83a55640413759165144a7
zhangbing1 <zhangbing1@yonyou.com>
2016-05-07 10:57:03 +0800

    除了action的封装其余的框架搭建完成

--- src/main/java/cn/cust/elec/web/action/ElecTextAction.java
+++ src/main/java/cn/cust/elec/web/action/ElecTextAction.java
@@ -1,18 +1,35 @@
 package cn.cust.elec.web.action;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 
 import cn.cust.elec.domain.ElecText;
+import cn.cust.elec.service.IElecTextService;
+import cn.cust.test.test;
 
+@SuppressWarnings("serial")
 @Controller
+@Scope(value="prototype")
 public class ElecTextAction extends ActionSupport implements ModelDriven<ElecText> {
 	
-	private static final long serialVersionUID = -6957518430159378978L;
+	@Autowired
+	private IElecTextService elecTextService;
+	private Log log = LogFactory.getLog(test.class);
 	ElecText elecText = new ElecText();
 	public String save(){
+		
+		try {
+			elecTextService.saveElecText(elecText);
+		} catch (Exception e) {
+			log.error(e.getMessage(),e);
+			e.printStackTrace();
+		}
 		return "save";
 	}
 
